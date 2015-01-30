a1251e17f03f5ceef556fe117f8982cc14eabf27
yangzm <yangzm@imaxpo.com>
2015-01-30 10:22:14 +0800

    depend show ok

--- mm.py
+++ mm.py
@@ -11,6 +11,9 @@ class MM:
     def __init__(self):
         self.mm_config_name = r"mm.cfg"
         self.mm_config = mmconfig.MMConfig()
+        self.mm_depends = []
+        self.mm_all_depends = {}
+        self.repo_dirs = []
 
         self.mm_path = os.path.dirname(__file__)
         self.scons_struct = os.path.join(self.mm_path, "sconstruct")
@@ -23,6 +26,15 @@ class MM:
         self.__init_config()
         self.build_dir = self.mm_config.get_value("env.build_dir", os.path.normpath(self.module_path))
         self.__mkdir(self.build_dir)
+
+        repos = self.mm_config.get_items("repo.local")
+        for repo in repos:
+            dir = self.mm_config.get_value("repo.local." + repo + ".dir")
+            print("repo %s dir is %s" % (repo, dir))
+            assert os.path.isdir(dir)
+            self.repo_dirs.append(dir)
+
+        self.mm_depends = self.mm_config.get_value("module.depend", "").split(',')
 
         self.scons_path = self.mm_config.get_value("scons.path", self.__find_file("scons"))
         self.scons_param = self.mm_config.get_value("scons.param")
@@ -40,6 +52,62 @@ class MM:
             if os.path.exists(config_file) and os.path.isfile(config_file):
                 print("mm  " + config_file)
                 self.mm_config.read_config(config_file)
+
+    def get_module_path(self, module_name):
+        for dir in self.repo_dirs:
+            path = os.path.join(dir, module_name)
+            if os.path.isdir(path):
+                return path
+        return None
+
+    def get_depends(self):
+        dict_depends = {}
+        for depend in self.mm_depends:
+            # print("depend -> %s" % depend)
+            dict_depends[depend] = self.__get_depends(depend)
+        self.mm_all_depends[self.module_name] = dict_depends
+        return self.mm_all_depends
+
+    def __show_map(self, base, map_data):
+        data = map_data.items()
+        while data:
+            k, v = data.pop(0)
+            sys.stdout.write('%s->%s' % (base, k))
+            items = v.items()
+            if len(items) == 1:
+                (ik, iv) = items[0]
+                if not iv.items():
+                    sys.stdout.write('->%s' % ik)
+                    sys.stdout.write('\n')
+                    continue
+            sys.stdout.write('\n')
+            if data:
+                self.__show_map(base + '|   ', v)
+            else:
+                self.__show_map(base + '    ', v)
+
+    def __get_depends(self, module_name):
+        mm_depends = {}
+        if module_name == self.module_name:
+            print("depend come back to self")
+            return mm_depends
+        mm_config = mmconfig.MMConfig()
+        module_path = self.get_module_path(module_name)
+        if module_path is None:
+            # print("can't find module " + module_name)
+            return mm_depends
+        # print("find module_path =" + module_path)
+        mm_config.read_config(os.path.join(module_path, self.mm_config_name))
+        depends_str = mm_config.get_value("module.depend", None)
+        if depends_str is None:
+            print("%s -> none " % module_name)
+            return mm_depends
+        depends = depends_str.split(',')
+        for depend in depends:
+            if depend != "":
+                # print("depend -> %s" % depend)
+                mm_depends[depend] = self.__get_depends(depend)
+        return mm_depends
 
     def __add_cmd_param(self, param, add_param):
         return param + " " + add_param + " ";
@@ -111,8 +179,9 @@ class MM:
         os.system(self.scons_path + scons_param)
 
     def show_depends(self):
-        depends = self.mm_config.get_value("module.depend", "").split(',')
-        print("Depend:" + depend for depend in depends)
+        depends_map = self.get_depends()
+        if depends_map is not None:
+            self.__show_map("", depends_map)
 
 
 def find_source(suffixlist, path='.', recursive=False):
@@ -150,6 +219,10 @@ if __name__ == "__main__":
     if "show" in sys.argv:
         mm.show_env()
         mm.show_depends()
-    mm.build_module(".")
+
+    print("")
+    print("")
+    print("*" * 20)
+    # mm.build_module(".")
 
 
